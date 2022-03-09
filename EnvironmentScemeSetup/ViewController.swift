18fd8114ad1c8e1c973e9c236d18a5eff3ae9cc5
nvadde <nvadde@uworld.in>
2022-03-09 06:53:23 +0530

    Environment setup

---
+++ EnvironmentScemeSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnvironmentScemeSetup
+//
+//  Created by UW-IN-LPT0108 on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
